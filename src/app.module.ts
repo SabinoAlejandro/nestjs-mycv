37c4bf026c7ab4c7b22b1a0cbe398ad1cce83011
Sabino Alejandro Arambula Rodriguez <arambularodriguezsabino@gmail.com>
2025-01-23 20:29:51 -0600

    E2E tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,44 @@
-import { Module } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-require-imports */
+import { Module, ValidationPipe, MiddlewareConsumer } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReportsModule } from './reports/reports.module';
 import { UsersModule } from './users/users.module';
 import { UserEntity } from './users/user.entity';
 import { ReportEntity } from './reports/report.entity';
+const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [UserEntity, ReportEntity],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          entities: [UserEntity, ReportEntity],
+          synchronize: true,
+        };
+      },
     }),
     ReportsModule,
     UsersModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    { provide: APP_PIPE, useValue: new ValidationPipe({ whitelist: true }) },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(cookieSession({ keys: ['rasssssssndom'] })).forRoutes('*');
+  }
+}
